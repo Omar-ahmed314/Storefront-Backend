80dae9dd34f44ab5f4ffd2468da70ff04d9f1154
Omar A.Rezk <62177195+Omar-ahmed314@users.noreply.github.com>
2022-09-29 13:40:48 +0200

    edit user model

--- src/models/order.ts
+++ src/models/order.ts
@@ -2,8 +2,6 @@ import Client from "../database";
 
 export type order = {
     id: number,
-    product_id: number,
-    quantity: number,
     user_id: number
 };
 
@@ -11,7 +9,7 @@ export default class Order {
     async index(): Promise<order[]> {
         try {
             const connection = await Client.connect();
-            const sql = 'SELECT * FROM orders_table';
+            const sql = 'SELECT * FROM order_table';
             const result = await connection.query(sql);
             connection.release();
             return result.rows;
@@ -23,7 +21,7 @@ export default class Order {
     async show(id: number): Promise<order[]> {
         try {
             const connection = await Client.connect();
-            const sql = 'SELECT * FROM orders_table WHERE id = ($1)';
+            const sql = 'SELECT * FROM order_table WHERE id = ($1)';
             const result = await connection.query(sql, [id]);
             connection.release();
             return result.rows;
@@ -35,12 +33,24 @@ export default class Order {
     async create(Order: order): Promise<order> {
         try {
             const connection = await Client.connect();
-            const sql = 'INSERT INTO orders_table (product_id, quantity, user_id) VALUES (($1), ($2), ($3))';
-            const result = await connection.query(sql, [Order.product_id, Order.quantity, Order.user_id]);
+            const sql = 'INSERT INTO order_table (user_id) VALUES (($1))';
+            const result = await connection.query(sql, [Order.user_id]);
             connection.release();
             return result.rows[0];
         } catch (error) {
             throw new Error(`Cannot create new order: error ${error}`);
+        }
+    }
+
+    async addProductToOrder(orderId: number, productId: number, quantity: number): Promise<{order_id: number, product_id: number, quantity: number}> {
+        try {
+            const connection = await Client.connect();
+            const sql = 'INSERT INTO order_product_table (order_id, product_id, quantity) VALUES (($1), ($2), ($3))';
+            const result = await connection.query(sql, [orderId, productId, quantity]);
+            connection.release();
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Cannot insert new product into order: error ${error}`);
         }
     }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -43,4 +43,28 @@ export default class User {
             throw new Error(`Cannot create new user: error ${error}`);
         }
     }
+
+    async edit(User: user): Promise<user> {
+        try {
+            const connection = await Client.connect();
+            const sql = 'UPDATE user_table SET first_name = ($1) AND last_name = ($2) AND password = ($3) WHERE id = ($4)';
+            const result = await connection.query(sql, [User.first_name, User.last_name, User.password, User.id]);
+            connection.release();
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Cannot edit the user: error ${error}`);
+        }
+    }
+
+    async delete(id: number): Promise<user> {
+        try {
+            const connection = await Client.connect();
+            const sql = 'DELETE FROM user_table WHERE id = ($1)';
+            const result = await connection.query(sql, [id]);
+            connection.release();
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Cannot delete the user: error ${error}`);
+        }
+    }
 }
