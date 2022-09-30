fd22e5044213ee9d256e90ed6d2afe0e9deb7529
Omar A.Rezk <62177195+Omar-ahmed314@users.noreply.github.com>
2022-09-30 19:59:36 +0200

    add services handler

---
+++ src/handlers/servicesHandler.ts
@@ -0,0 +1,22 @@
+import miscQueries from '../services/services';
+import { Response, Request, Application } from 'express';
+
+const miscQueriesModel = new miscQueries();
+
+// endpoints
+const incomplitedOrders = async (req : Request, res: Response) => {
+   try {
+       const data = await miscQueriesModel.getAllIncomplitedOrders(req.body.id);
+       res.status(200);
+       res.json(data);
+   } catch (error) {
+    res.status(404);
+    res.send(error);
+   }
+}
+
+const userRoutes = (app: Application) => {
+    app.get('/all_incomplited_orders', incomplitedOrders);
+}
+
+export default userRoutes;
