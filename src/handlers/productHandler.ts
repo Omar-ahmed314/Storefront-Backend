a9beb161fd2b1eb0806ed2f82d6334f771f556a4
Omar A.Rezk <62177195+Omar-ahmed314@users.noreply.github.com>
2022-09-30 14:58:41 +0200

    add product handler

---
+++ src/handlers/productHandler.ts
@@ -0,0 +1,80 @@
+import Product, {product} from "../models/product";
+import { Response, Request, Application } from 'express';
+
+const productModel = new Product();
+
+// endpoints
+const index = async (req : Request, res: Response) => {
+   try {
+       const data = await productModel.index();
+       res.status(200);
+       res.json(data);
+   } catch (error) {
+    res.status(500);
+    res.send(error);
+   }
+}
+
+const show = async (req : Request, res: Response) => {
+    try {
+        const data = await productModel.show(req.params.id as unknown as number);
+        res.status(200);
+        res.json(data);
+    } catch (error) {
+     res.status(500);
+     res.send(error);
+    }
+ }
+
+const create = async (req: Request, res: Response) => {
+    try {
+        const productData: product = {
+            id: 1,
+            title: req.body.title,
+            price: req.body.price
+        };
+        const data = await productModel.create(productData);
+        res.status(200);
+        res.json(data);
+    } catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+}
+
+const edit = async (req: Request, res: Response) => {
+    try {
+        const productData: product = {
+            id: req.body.id,
+            title: req.body.title,
+            price: req.body.price
+        };
+        const data = await productModel.edit(productData);
+        res.status(200);
+        res.json(data);
+    } catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+}
+
+const _delete = async (req: Request, res: Response) => {
+    try {
+        const data = await productModel.delete(req.params.id as unknown as number);
+        res.status(200);
+        res.json(data);
+    } catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+}
+
+const productRoutes = (app: Application) => {
+    app.get('product', index);
+    app.get('product/:id', show);
+    app.post('product', create);
+    app.put('product', edit);
+    app.delete('product/:id', _delete);
+}
+
+export default productRoutes;
